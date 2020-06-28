cd66c9bb8761e89338b8f4c8bb5d192aa96967ad
EC2 Default User <ec2-user@ip-10-0-1-113.us-east-2.compute.internal>
2020-06-28 19:42:38 +0000

    script update

--- startup-scripts/start.sh
+++ startup-scripts/start.sh
@@ -6,16 +6,18 @@
 # Install packages
 yum -y update 
 yum install -y git 
-yum install -y tree python3
-curl -O https://bootstrap.pypa.io/get-pip.py
-yum -y install python-pip
+yum install - y postgresql
+yum install -y postgresql
 yum install -y gcc
-pip install psycopg2-binary
-pip install psycopg2
-pip install ansible
-yum install -y postgresql 
-pip install boto3 --user
+yum install -y python3-devel
+yum install -y postgresql-devel
+yum install -y jq # json parser
+pip3 install psycopg2
+pip3 install boto3
 
+cd /home/ec2-user
+pip3 install --user boto3
+pip3 install --user psycopg2
 
 cd /home/ec2-user 
 git clone https://github.com/mlukacsko/java-image-gallery.git 
@@ -23,6 +25,8 @@ git clone https://github.com/mlukacsko/ansible.git
 chown -R ec2-user:ec2-user java-image-gallery
 chown -R ec2-user:ec2-user ansible
 
+cd /home/ec2-user/ansible
+ansible-playbook postgres.yaml
 
 # Start/enable services
 systemctl stop postfix 
