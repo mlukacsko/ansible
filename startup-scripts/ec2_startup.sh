65d4a5b36c898cf7071906f0e8b7c68c989a7020
EC2 Default User <ec2-user@ip-10-0-1-137.us-east-2.compute.internal>
2020-06-29 02:12:49 +0000

    start up script

--- startup-scripts/ec2_startup.sh
+++ startup-scripts/ec2_startup.sh
@@ -14,7 +14,7 @@ sudo pip3 install ansible
 sudo pip3 install boto3 --user
 sudo pip3 install psycopg2 --user
 cd /home/ec2-user
-git clone https://github.com/cdavidshaffer/java-image-gallery.git
+git clone https://github.com/mlukacsko/java-image-gallery.git
 git clone https://github.com/mlukacsko/ansible.git
 chown -R ec2-user:ec2-user java-image-gallery
 chown -R ec2-user:ec2-user ansible
