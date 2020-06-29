e3f4d301f9a9e96ec687b2d1eb9a837724e1e658
EC2 Default User <ec2-user@ip-10-0-1-40.us-east-2.compute.internal>
2020-06-29 00:24:02 +0000

    startup scripts

--- startup-scripts/ec2_startup.sh
+++ startup-scripts/ec2_startup.sh
@@ -3,4 +3,4 @@
 
 export IMAGE_GALLERY_BOOTSTRAP_VERSION="1.0"
 aws s3 cp s3://edu.au.cc.image-gallery-configuration/ec2-prod-latest.sh ./
-/usr/bin/bash ec2-prod-latest.sh
+/usr/bin/bash ec2-prod-1.0.sh
