a942279e0e9a7c725af1e3a3bbbefe9577b99a98
EC2 Default User <ec2-user@ip-10-0-1-64.us-east-2.compute.internal>
2020-06-29 01:26:24 +0000

    misc changes

--- startup-scripts/ec2_startup.sh
+++ startup-scripts/ec2_startup.sh
@@ -5,6 +5,7 @@ sudo yum install -y gcc
 sudo yum install -y python3-devel
 sudo yum install -y postgresql-devel
 sudo yum install -y jq # json parser
+sudo yum install -y git
 sudo pip3 install psycopg2
 sudo pip3 install boto3
 cd /home/ec2-user
